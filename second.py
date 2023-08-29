496eb1fee09563be0b7327467359ce99cef25db3
sleepyemmmmm <142364854+sleepyemmmmm@users.noreply.github.com>
2023-08-29 18:45:27 +0800

    Update second.py

--- second.py
+++ second.py
@@ -88,3 +88,21 @@ class AreaCalculatorApp:
             area *= 2.54 ** 2
 
         self.result_label.config(text=f"{shape}，面积为：{area:.3f} 平方厘米")
+
+def calculate_square_area(side):
+    return side ** 2
+
+def calculate_rectangle_area(length, width):
+    return length * width
+
+def calculate_triangle_area(base, height):
+    return 0.5 * base * height
+
+def calculate_circle_area(diameter):
+    radius = diameter / 2
+    return math.pi * (radius ** 2)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AreaCalculatorApp(root)
+    root.mainloop()
